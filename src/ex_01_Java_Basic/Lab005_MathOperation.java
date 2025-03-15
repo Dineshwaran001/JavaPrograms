8b20a2714390ca4983e12a4edde197f43af56589
dineshwaran.t <warandinesh33@gmail.com>
2025-03-15 14:47:47 +0530

    15March afternoon

---
+++ src/ex_01_Java_Basic/Lab005_MathOperation.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+
+
+public class Lab005_MathOperation {
+    public static void main(String[] args) {
+        int a =5;
+        int b= 2;
+        System.out.println(a + b);
+        System.out.println(a*b);
+        System.out.println(a/b);// this will not return 2.5 because sout funttion only return int not double or decimal values
+        System.out.println(a-b);
+    }
+
+}
--- src/ex_01_Java_Basic/lab004_KeywordIdentifiers.java
+++ src/ex_01_Java_Basic/lab004_KeywordIdentifiers.java
@@ -1,6 +1,6 @@
 package ex_01_Java_Basic; //this name is also identifier
 
-public class lab004_KeywordIdentifiers // this is identifier
+public class lab004_KeywordIdentifiers // this name is identifier
  {
      public static void main(String[] args) {
          System.out.println("Hi");
