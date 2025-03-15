ec9bcd3c8acc7177aaa664f4434d3abda25ac0c8
dineshwaran.t <warandinesh33@gmail.com>
2025-03-15 10:08:40 +0530

    15March

---
+++ src/ex_01_Java_Basic/lab003_CommentsSingleMultiple.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class lab003_CommentsSingleMultiple {
+
+    public static void main(String[] args) {
+        //this is single line comment-
+
+        /*
+        * these are used for Multiple line comments
+        */
+    }
+}
---
+++ src/ex_01_Java_Basic/lab004_KeywordIdentifiers.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic; //this name is also identifier
+
+public class lab004_KeywordIdentifiers // this is identifier
+ {
+     public static void main(String[] args) {
+         System.out.println("Hi");
+     }
+
+}
