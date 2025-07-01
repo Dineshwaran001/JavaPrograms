cf175b0402a833c46b3609b3666d57e96db6cc28
dineshwaran.t <warandinesh33@gmail.com>
2025-07-01 20:18:18 +0530

    01/07/2025

--- src/ex_25_Interface_Abstraction/Lab162_Interface_Abstraction.java
+++ src/ex_25_Interface_Abstraction/Lab162_Interface_Abstraction.java
@@ -1,5 +1,6 @@
 package ex_25_Interface_Abstraction;
 
+
 interface Father{
     void Money(); // interface only have the incomplete method
  }
@@ -7,11 +8,16 @@ interface Mother{
     void Money();
 }
 
-class child implements Father, Mother{ // multiple inheritance can be achieved by interface
+class Child implements Father, Mother{ // multiple inheritance can be achieved by interface
 
 
     @Override // ex_25_Interface_Abstraction.child method is going to complete the method
     public void Money() {
-        System.out.println("ex_25_Interface_Abstraction.child money");
+        System.out.println("child money");
+    }
+
+    public static void main(String[] args) {
+        Child ch = new Child();
+        ch.Money();
     }
 }
--- src/ex_25_Interface_Abstraction/Lab164_ExampleInterfaceA.java
+++ src/ex_25_Interface_Abstraction/Lab164_ExampleInterfaceA.java
@@ -1,13 +1,17 @@
 package ex_25_Interface_Abstraction;
 
 public class Lab164_ExampleInterfaceA {
+    public static void main(String[] args) {
+        element ele = new element();
+        ele.display();
+    }
 }
 
 class element extends IncompleteClass implements IncompleteInterface{
 
     @Override
     public void display() {
-        System.out.println("we have rite our oun code here ");
+        System.out.println("we have write our oun code here ");
     }
 }
 
