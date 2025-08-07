e9b6dbba3e208a6d8da3eda79593fafeecb72dff
dineshwaran.t <warandinesh33@gmail.com>
2025-08-07 20:49:11 +0530

    07/08/2025

--- src/ex_28_WrapperClass/Lab177_Wrapper_eg.java
+++ src/ex_28_WrapperClass/Lab177_Wrapper_eg.java
@@ -1,21 +1,42 @@
-package ex_28_WrapperClass;
-
 public class Lab177_Wrapper_eg {
 }
 class Mobile{
-    public Mobile(Integer no, String name, Double price) {
+
+
+    private Integer no;
+
+    public Integer getNo() {
+        return no;
+    }
+
+    public void setNo(Integer no) {
         this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
         this.price = price;
     }
 
-    private Integer no;
     private String name;
     private Double price;
 }
 Mobile(){
     System.out.println("DC");
 }
-public Mobile(Integer no, String name, Double price ){
-    this.no
+public Mobile(Integer no, String name, Double price) {
+    this.no = no;
+    this.name = name;
+    this.price = price;
 }
